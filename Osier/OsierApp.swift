9c0cd397b4984005ce7ab41af13aa6ccaf8e18b8
vryghd <christianh1218@gmail.com>
2026-04-08 02:22:31 -0500

    Initial Commit

---
+++ Osier/OsierApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsierApp.swift
+//  Osier
+//
+//  Created by Christian Hill on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct OsierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
